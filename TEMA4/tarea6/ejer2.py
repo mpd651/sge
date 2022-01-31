d792f55ca45174de9e69b00bd5e28353351fb15d
nombre_usuario <email_id>
2022-01-31 16:46:30 +0100

    clase

---
+++ TEMA4/tarea6/ejer2.py
@@ -0,0 +1,9 @@
+print('Introduce el nombre de una persona: (* para salir)')
+persona=input()
+print('Introduce una aficcion de la persona')
+aficcion=input()
+diccionario={}
+
+while (persona!="*"):
+    if diccionario
+    diccionario[persona]=aficcion
---
+++ TEMA4/tarea7/ejer1.py
@@ -0,0 +1,29 @@
+import os
+
+def funcionLeerEscribir (ficheroNombre):
+    with open(ficheroNombre, "a+") as fichero:
+        print('Desea editar el archivo?(salir)')
+        opcion=input()
+        while opcion!='salir':
+            print('Introduce texto a concateear en el fichero:')
+            texto=input()
+            fichero.write("\n")
+            fichero.write(texto)
+            print('Desea editar el archivo?(salir):')
+            opcion=input()
+        contenido=fichero.read()
+        print (contenido)
+    fichero.closed
+
+print('Introduce un nombre de fichero: ')
+ficheroNombre=input()
+if os.path.exists(ficheroNombre):
+    funcionLeerEscribir (ficheroNombre)
+
+else:
+    print('El fichero no existe')
+    funcionLeerEscribir (ficheroNombre)
+
+
+
+
