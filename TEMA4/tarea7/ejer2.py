82fc56089260667df20eefd94f546d84b613b0b5
mario <mpd651.@gmail.com>
2022-02-01 20:56:45 +0100

    casa

--- TEMA4/tarea7/ejer2.py
+++ TEMA4/tarea7/ejer2.py
@@ -1,3 +1,7 @@
+import csv
+import json
+
+
 print('MENU:')
 print('1 Listar datos\n2 Crear contacto\n3 Borrar contacto\n4 Guardar CSV\n5 Guardar JSON\n6 Cargar CSV\n7 Cargar JSON\n 0 Salir')
 opcion=int(input())
@@ -32,13 +36,40 @@ while opcion!=0:
                 
         
     if opcion==4:
-        continue
+        print('Introduce nombre del fichero a guardar CSV:')
+        nombre=input()
+        fichero=open(nombre+'.csv')
+        contenido=csv.writer(fichero)
+        contenido.writerows(contactos)
+        fichero.close()
+
     if opcion==5:
-        continue
+        print('Introduce nombre del fichero a guardar JSON:')
+        nombre=input()
+        fichero=open(nombre+'.json')
+        json.dump(contactos,fichero)
+        fichero.close()
+
     if opcion==6:
-        continue
+        print('Introduce nombre del fichero a leer CSV:')
+        nombre=input()
+        fichero=open(nombre+'.csv')
+        contenido=csv.reader(fichero)
+        list(contenido)
+        for fila in contenido:
+            print ('Fila '+str(contenido.line_num)+' '+str(fila))
+        fichero.close()
+
     if opcion==7:
-        continue
+        print('Introduce nombre del fichero a leer JSON:')
+        nombre=input()
+        fichero=open(nombre+'.csv')
+        datos=json.loads(fichero)
+        datos
+        for i in datos:
+            print (i)
+        fichero.close()
+        
     
     print('MENU:')
     print('1 Listar datos\n2 Crear contacto\n3 Borrar contacto\n4 Guardar CSV\n5 Guardar JSON\n6 Cargar CSV\n7 Cargar JSON\n 0 Salir')
